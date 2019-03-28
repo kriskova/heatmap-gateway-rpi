2cc73623b024c318ea411276946777a89f93906d
kriskova <kriskovamail@gmail.com>
2019-03-28 21:39:01 +0100

    Add linux serial path

--- start.rb
+++ start.rb
@@ -2,7 +2,8 @@ require 'rubygems'
 require 'serialport'
 require 'unirest'
 
-PORT_PATH = '/dev/cu.usbmodem1452401'
+MAC_PATH = '/dev/cu.usbmodem1452401'.freeze
+LINUX_PATH = '/dev/ttyACM0'.freeze
 BAUD_RATE = 9600
 DATA_BITS = 8
 STOP_BITS = 1
@@ -10,7 +11,11 @@ PARITY = SerialPort::NONE
 
 MESSAGE_REGEX = /nodeid: [0-9]{1,3} temp: [0-9]{1,2}.[0-9]{2} humidity: [0-9]{1,2}.[0-9]{2}/
 
-serial = SerialPort.new(PORT_PATH, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY)
+def serial_path
+  File.exists?(MAC_PATH) ? MAC_PATH : LINUX_PATH
+end
+
+serial = SerialPort.new(serial_path, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY)
 
 loop do
   puts 'Waiting for data...'
