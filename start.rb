60563b3689b65d6f7552108e706668dc9b279146
kriskova <kriskovamail@gmail.com>
2019-03-28 22:40:55 +0100

    Add FTDI support

--- start.rb
+++ start.rb
@@ -3,7 +3,8 @@ require 'serialport'
 require 'unirest'
 
 MAC_PATH = '/dev/cu.usbmodem1452401'.freeze
-LINUX_PATH = '/dev/ttyACM0'.freeze
+LINUX_ARDUINO_PATH = '/dev/ttyACM0'.freeze
+LINUX_FTDI_PATH = '/dev/ttyUSB0'.freeze
 BAUD_RATE = 9600
 DATA_BITS = 8
 STOP_BITS = 1
@@ -13,10 +14,22 @@ HOST = 'https://creative-capital-2019.herokuapp.com'.freeze
 MESSAGE_REGEX = /nodeid: [0-9]{1,3} temp: [0-9]{1,2}.[0-9]{2} humidity: [0-9]{1,2}.[0-9]{2}/
 
 def serial_path
-  File.exists?(MAC_PATH) ? MAC_PATH : LINUX_PATH
+  if File.exists? MAC_PATH
+    MAC_PATH
+  elsif File.exists? LINUX_ARDUINO_PATH
+    LINUX_ARDUINO_PATH
+  elsif File.exists? LINUX_FTDI_PATH
+    LINUX_FTDI_PATH
+  else
+    fail 'No supported devices found'
+  end
 end
 
-serial = SerialPort.new(serial_path, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY)
+def baud_rate
+  serial_path == LINUX_FTDI_PATH ? 115200 : 9600
+end
+
+serial = SerialPort.new(serial_path, baud_rate, DATA_BITS, STOP_BITS, PARITY)
 
 loop do
   puts 'Waiting for data...'
