f4e3882e39f7977690ba6c015b9ad8018960b4f9
kriskova <kriskovamail@gmail.com>
2019-03-28 21:26:06 +0100

    Improve message parsing

--- start.rb
+++ start.rb
@@ -8,18 +8,28 @@ DATA_BITS = 8
 STOP_BITS = 1
 PARITY = SerialPort::NONE
 
+MESSAGE_REGEX = /nodeid: [0-9]{1,3} temp: [0-9]{1,2}.[0-9]{2} humidity: [0-9]{1,2}.[0-9]{2}/
+
 serial = SerialPort.new(PORT_PATH, BAUD_RATE, DATA_BITS, STOP_BITS, PARITY)
 
 loop do
   puts 'Waiting for data...'
   line = serial.readline("\r")
-  parsed = line.scan(/([0-9]{1,2}.[0-9]{2})/)
 
-  # [Humidity, Temp]
-  puts parsed.flatten.inspect
-  response = Unirest.post "http://httpbin.org/post",
-                        headers: { "Accept" => "application/json" },
-                        parameters: { humidity: parsed.first, temp: parsed.last }
-  puts response.code
+  if MESSAGE_REGEX.match? line
+    # nodeid: 100 temp: 25.60 humidity: 54.80
+    # nodeid: 100 temp: nan humidity: nan
+    parsed = line.split
+
+    # node, temp, humidity
+    puts parsed.inspect
+    response = Unirest.post "http://httpbin.org/post",
+      headers: { "Accept" => "application/json" },
+      parameters: { humidity: parsed[3], temp: parsed[5] }
+    puts response.code
+  else
+    puts 'Unrecognized message format'
+  end
+
   sleep 1
 end
